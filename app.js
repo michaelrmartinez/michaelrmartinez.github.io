17280779282949b3086e769da598745f9cd6f483
michaelrmartinez <mirima92@gmail.com>
2021-03-31 23:56:59 -0400

    set up base project files; created responsive navbar

---
+++ app.js
@@ -0,0 +1,15 @@
+// VARIABLES
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+const body = document.querySelector('body');
+
+
+// Display mobile menu
+const mobileMenu = function () {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    body.classList.toggle('active');
+}
+
+menu.addEventListener('click', mobileMenu);
