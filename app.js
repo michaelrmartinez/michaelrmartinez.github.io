5786607cac1a853fa61addd622a4e083632617e8
michaelrmartinez <mirima92@gmail.com>
2021-04-04 00:50:59 -0400

    made mobile navmenu close when clicking link; added portfolio section; refactored code for wider use (container, heading, topline)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
 const body = document.querySelector('body');
+const navLinks = document.querySelectorAll('.navbar__link');
 
 
 // Display mobile menu
@@ -12,4 +13,15 @@ const mobileMenu = function () {
     body.classList.toggle('active');
 }
 
+// Close menu
+const closeMenu = function () {
+    menuLinks.classList.remove('active');
+}
+
+
+// EVENT LISTENERS
 menu.addEventListener('click', mobileMenu);
+
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click', closeMenu);
+});
