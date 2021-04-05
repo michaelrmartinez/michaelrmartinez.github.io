520f9666332ba0d683ae281ed46261116c4d1107
michaelrmartinez <mirima92@gmail.com>
2021-04-04 22:02:43 -0400

    updated portfolio section with links and responsiveness

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-// VARIABLES
+// VARIABLES ----------
+// navmenu
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
 const body = document.querySelector('body');
 const navLinks = document.querySelectorAll('.navbar__link');
 
-
+// FUNCTIONS ----------
 // Display mobile menu
 const mobileMenu = function () {
     menu.classList.toggle('is-active');
@@ -21,7 +22,7 @@ const closeMenu = function () {
 }
 
 
-// EVENT LISTENERS
+// EVENT LISTENERS ----------
 menu.addEventListener('click', mobileMenu);
 
 navLinks.forEach(navLink => {
